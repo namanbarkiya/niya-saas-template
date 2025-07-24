a786f3e869ee5398a0eadb4e6f8d42595a4db84e
Naman <naman.barkiya02@gmail.com>
2025-07-24 17:59:41 +0530

    added scroll progess

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Hero from "@/components/Hero";
 import BentoDemo from "@/components/bentoGridComponent";
 import { SectionHeader } from "@/components/ui/SectionHeader";
+import { ScrollProgress } from "@/components/magicui/scroll-progress";
 
 export default function LandingPage() {
     return (
         <div className="flex flex-col items-center justify-center max-w-screen-lg mx-auto px-4 sm:px-6 md:px-8">
+            <ScrollProgress />
             <Hero />
             <SectionHeader title="Features for you" className="my-16" />
             <BentoDemo />
